c16a1fec2efef26eb8453a0eea8ce5d916591ef4
kunal-opash <kmo.opash@gmail.com>
2023-03-03 10:17:30 +0530

    Create Components and store greeting.js File

---
+++ src/components/Greeting.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Greeting = () => {
+  let curDate = new Date(2023, 3, 2, 22);
+  // console.log(date);
+  curDate = curDate.getHours();
+  let greeting = "";
+  const cssStyle = {};
+
+  if (curDate > 1 && curDate < 12) {
+    greeting = "Good Morning";
+    cssStyle.color = "Green";
+  } else if (curDate >= 12 && curDate < 20) {
+    greeting = "Good Afternoon";
+    cssStyle.color = "Orange";
+  } else {
+    greeting = "Good Night";
+    cssStyle.color = "Black";
+  }
+  return (
+    <div className="container">
+      <h2>
+        Hello @All,<span style={cssStyle}> {greeting}</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Greeting;
